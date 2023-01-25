ed65e8376c81da1c25ec7ee8ae0cb01609178932
conn6 <67378230+conn6@users.noreply.github.com>
2023-01-25 20:53:15 -0300

    Add files via upload

---
+++ portfolio/html/script.js
@@ -0,0 +1,7 @@
+function clickMenu() {
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }
+} //se o menu for igual a bloqueado, o click vai receber none e ser escondido
